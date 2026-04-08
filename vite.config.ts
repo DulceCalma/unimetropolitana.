a22364ee4357b3d38c059a8129271db809c720b1
Dulce Camla <agentvipers12@gmail.com>
2026-04-08 12:12:36 -0500

    V2

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/unimetropolitana/", // <--- AÑADE ESTA LÍNEA AQUÍ
   server: {
     host: "::",
     port: 8080,
